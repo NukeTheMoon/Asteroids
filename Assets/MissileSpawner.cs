1773573bbdfbb54fd023295029b73eb1efa92a3c
m00n <arcybartek@gmail.com>
2016-03-10 11:33:34 +0100

    Flying and shooting

---
+++ Assets/MissileSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileSpawner : MonoBehaviour
+{
+    public Transform World;
+    public GameObject MissilePrefab;
+    public float MissileSpeed = 1000.0f;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+            var missile = (GameObject) Instantiate(MissilePrefab, transform.position, transform.parent.rotation);
+	        missile.transform.parent = World;
+            PropelForward(missile);
+	    }
+	}
+
+    void PropelForward(GameObject missile)
+    {
+        var direction = transform.parent.forward;
+        Debug.Log(direction);
+        var rigidbody = missile.GetComponent<Rigidbody>();
+        rigidbody.AddForce(direction * MissileSpeed);
+        rigidbody.drag = 0;
+    }
+}
