7a83d7d7ddd80e755c8645bf5b7a6a4811d02570
m00n <arcybartek@gmail.com>
2016-03-19 17:32:55 +0100

    Added world reference

---
+++ Assets/Missile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour
+{
+    public WorldReference WorldReference;
+
+    private CapsuleCollider _collider;
+    private bool _selfdestruct = false;
+
+
+    void Start()
+    {
+        _collider = GetComponent<CapsuleCollider>();
+    }
+	
+	void Update ()
+	{
+	    _selfdestruct = true;
+	    if (_collider.bounds.Intersects(WorldReference.VisibleWorld.bounds))
+	    {
+	        _selfdestruct = false;
+	    }
+	    if (_selfdestruct)
+	    {
+	        Destroy(gameObject);
+	    }
+	}
+}
